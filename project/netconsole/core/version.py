917d538b3f6866daa681b66826fb7b660b3f16e6
wxj183589 <wxj183589@users.noreply.github.com>
2026-06-18 00:28:49 +0800

    auto release build

--- project/netconsole/core/version.py
+++ project/netconsole/core/version.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 
-APP_VERSION = "v1.0.1"
-BUILD_TIME = "2026-06-18 00:27:09"
-GIT_COMMIT = "9a5fabe"
+APP_VERSION = "v1.0.2"
+BUILD_TIME = "2026-06-18 00:28:49"
+GIT_COMMIT = "02f4513"
 APP_AUTHOR = "梦游"
 REPOSITORY_URLS = (
     "https://nas.love-ok.com:3021/mengyou/NetConsole.git",
