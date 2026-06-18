2ee3e2cc453ee6381430d3f0c8842700c3c321f0
wxj183589 <wxj183589@users.noreply.github.com>
2026-06-18 13:01:00 +0800

    auto release build

--- netconsole/core/version.py
+++ netconsole/core/version.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 
-APP_VERSION = "v1.0.11"
-BUILD_TIME = "2026-06-18 12:38:48"
-GIT_COMMIT = "c07b3e4"
+APP_VERSION = "v1.0.12"
+BUILD_TIME = "2026-06-18 13:01:00"
+GIT_COMMIT = "60dcd68"
 APP_AUTHOR = "姊︽父"
 REPOSITORY_URLS = (
     "https://nas.love-ok.com:3021/mengyou/NetConsole.git",
