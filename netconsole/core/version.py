7cfece3906792e7ad23e4d757b4a3ae0d664ce67
wxj183589 <wxj183589@users.noreply.github.com>
2026-06-23 22:27:55 +0800

    自动发布：更新版本、更新日志与构建文件

--- netconsole/core/version.py
+++ netconsole/core/version.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 
 APP_NAME = "NetConsole"
-APP_VERSION = "v1.3"
+APP_VERSION = "v1.2.3"
 APP_VERSION_DISPLAY = APP_VERSION
-BUILD_TIME = "2026-06-22 20:58:52"
-GIT_COMMIT = "72b5fa85"
+BUILD_TIME = "2026-06-23 22:27:54"
+GIT_COMMIT = "6fc16676"
 APP_AUTHOR = "梦游"
 REPOSITORY_URLS = (
     "https://nas.love-ok.com:3021/mengyou/NetConsole.git",
