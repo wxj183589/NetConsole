1861060eca7ca9b64a76d4aac462ac03d6cfb5f9
wxj183589 <wxj183589@users.noreply.github.com>
2026-07-24 06:30:36 +0800

    自动发布：更新版本、更新日志与构建文件

--- src/netconsole/core/version.py
+++ src/netconsole/core/version.py
@@ -6,16 +6,15 @@ APP_VERSION = "v1.4.2"
 APP_VERSION_DISPLAY = APP_VERSION
 APP_BYLINE = "by WXJ"
 APP_TITLE_DISPLAY = f"{APP_NAME} {APP_VERSION_DISPLAY} {APP_BYLINE}"
-BUILD_TIME = "2026-07-22 06:01:24"
-GIT_COMMIT = "0288ba1d"
+BUILD_TIME = "2026-07-24 06:30:36"
+GIT_COMMIT = "8d3b74d9"
 APP_AUTHOR = "梦游"
 REPOSITORY_PUSH_URLS = (
-    "git@github.com:wxj183589/NetConsole.git",
     "ssh://git@nas.love-ok.com:3022/mengyou/NetConsole.git",
+    "git@github.com:wxj183589/NetConsole.git",
 )
 REPOSITORY_WEB_URLS = (
     "https://github.com/wxj183589/NetConsole.git",
     "https://nas.love-ok.com:3021/mengyou/NetConsole.git",
 )
-# 兼容旧调用；用户界面只能使用可由浏览器打开的 HTTPS 地址。
 REPOSITORY_URLS = REPOSITORY_WEB_URLS
